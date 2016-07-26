992bb559f78c45396aac418dffd590b9efcbd3b3
Rocotton <Robert.Cotton@labor.idaho.gov>
2016-07-26 13:43:47 -0600

    Adding test for typescript files and fixing build

--- travis-test.sh
+++ travis-test.sh
@@ -7,9 +7,12 @@ gulp clean
 echo 'Creating javascript folder'
 mkdir -p wwwroot/lib/jquery/dist/
 
+mkdir -p Scripts/
+
 echo "Writing javascript file"
 echo "function hellowWorld() { alert('Hello World'); }" > wwwroot/lib/jquery/dist/jquery.js
 echo "function hellowWorld() { alert('Hello World'); }" > wwwroot/lib/jquery/dist/jquery.min.js
+echo "function hellowWorld() { alert('Hello World'); }" > Scripts/App.ts
 
 echo 'Creating css folder'
 mkdir -p wwwroot/lib/pure/
@@ -25,6 +28,12 @@ echo "asserting javascript files were created"
 if [ ! -f 'wwwroot/js/site.js' ]; then 
 	echo "Site.js not created" 
 	exit 1 
+fi
+
+echo "asserting typescript files were created"
+if [ ! -f 'wwwroot/js/Scripts/App.js' ]; then
+	echo "Scripts/App.js not created"
+	exit 1
 fi
 
 echo "asserting css files were created"
